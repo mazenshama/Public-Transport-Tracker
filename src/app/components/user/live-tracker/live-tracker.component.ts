4c0e8c9edac0c7c551bf76b917aeaef20aa1ab23
SalmaElhennawi <salma.k.elhennawi@gmail.com>
2025-10-25 19:27:10 +0300

    "added structure (folders & components)"

---
+++ src/app/components/user/live-tracker/live-tracker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-live-tracker',
+  standalone: true,
+  imports: [],
+  templateUrl: './live-tracker.component.html',
+  styleUrl: './live-tracker.component.css'
+})
+export class LiveTrackerComponent {
+
+}
