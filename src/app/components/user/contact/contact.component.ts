1809a2d70064bd698eec2d22614aa90a1fe333cc
SalmaElhennawi <salma.k.elhennawi@gmail.com>
2025-10-30 23:59:42 +0200

    "created contact"

--- src/app/components/user/contact/contact.component.ts
+++ src/app/components/user/contact/contact.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+ contact = {
+    name: '',
+    email: '',
+    phone: '',
+    subject: '',
+    message: ''
+  };
 
+  sendMessage() {
+    console.log('Message sent:', this.contact);
+    alert('Your message has been sent!');
+  }
 }
