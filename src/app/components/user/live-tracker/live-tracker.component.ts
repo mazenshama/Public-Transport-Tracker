471aed4ae73ea1d15697e3eebf4366bf32a7c2e8
SalmaElhennawi <salma.k.elhennawi@gmail.com>
2025-11-06 18:09:24 +0200

    "Live Tracker UI"

--- src/app/components/user/live-tracker/live-tracker.component.ts
+++ src/app/components/user/live-tracker/live-tracker.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-live-tracker',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './live-tracker.component.html',
   styleUrl: './live-tracker.component.css'
 })
 export class LiveTrackerComponent {
-
+ buses = [
+    { number: 12, description: 'ayhaga dlwaaty' },
+    { number: 12, description: 'ayhaga dlwaaty' },
+    { number: 12, description: 'ayhaga dlwaaty' },
+    { number: 12, description: 'ayhaga dlwaaty' },
+    { number: 12, description: 'ayhaga dlwaaty' },
+  ];
 }
