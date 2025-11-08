9adb7a11e10d8c2bac9fd2072abcb5bed613688c
monicamaged5 <monicamaged5@gmail.com>
2025-11-08 22:12:40 +0200

    Solved Non Injector Error

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { Route } from '@angular/router';
+import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []),
+    provideRouter(routes),
+    provideHttpClient() 
+  ]
+}).catch((err) => console.error(err));
+
