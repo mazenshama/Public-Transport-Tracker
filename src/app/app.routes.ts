c07d1a5a8acd61fd3f0928c8b6c27c4fc75725c9
monicamaged5 <monicamaged5@gmail.com>
2025-11-08 20:23:32 +0200

    Update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,14 +10,18 @@ import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent, title: 'Home' },
+  {path:'',redirectTo:'home',pathMatch:'full'},
+
+  {path:'login',component:LoginComponent, title: 'login' },
+  {path:'register',component:RegisterComponent, title: 'register'},
+  
+  { path: 'home', component: HomeComponent, title: 'Home' },
   { path: 'live-tracker', component: LiveTrackerComponent, title: 'Live Tracker' },
   { path: 'routes', component: RoutingComponent, title: 'Routes' },
   { path: 'contact', component: ContactComponent, title: 'Contact Us' },
   { path: 'admin', component: AdminDashboardComponent, title: 'Admin Dashboard' },
   { path: 'driver', component: DriverDashboardComponent, title: 'Driver Dashboard' },
-  { path: 'login', component: LoginComponent, title: 'login-Authentication' },
-  { path: 'Register', component: RegisterComponent, title: 'Register-Authentication' },
+
 
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
