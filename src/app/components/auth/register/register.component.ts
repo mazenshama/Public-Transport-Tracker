df44c95adfeb4eb58cb85e83a9d57a040aaade81
monicamaged5 <monicamaged5@gmail.com>
2025-11-08 20:22:04 +0200

    Register Form

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,12 +1,35 @@
+import { AuthServices } from './../../../../../Services/auth.service';
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup,Validators,FormControl } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,NgIf,RouterModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  //  constructor( private _AuthServices:AuthServices){}
+regForm: FormGroup = new FormGroup({
+    name: new FormControl('',[Validators.required]),
+    email: new FormControl('',[Validators.required,Validators.email]),
+    password: new FormControl('',[Validators.required,Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/)]),
+    confirmPassword: new FormControl('',[Validators.required,Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/)]),
+    role: new FormControl('',[Validators.required])
+  })
 
+  SignUp(){
+    // if(this.regForm.valid){
+    //    this._AuthServices.serLogIn(this.regForm.value).subscribe({
+    //   next:(res)=>{
+    //         console.log(res);
+
+    //   }
+    // })
+    // }
+  }
 }
