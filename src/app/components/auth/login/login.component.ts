f036d0a262e3adb2de2f6bf53a381381f185dd5e
monicamaged5 <monicamaged5@gmail.com>
2025-11-08 20:21:41 +0200

    Login Form

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,12 +1,41 @@
+import { AuthServices } from './../../../../../Services/auth.service';
 import { Component } from '@angular/core';
+import {FormControl,FormGroup,ReactiveFormsModule, Validators } from '@angular/forms'
+import { NgIf } from '@angular/common' 
+import { Router, RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf,RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  // constructor( private _AuthService:AuthService, private _Router:Router){}
+      constructor( private _Router:Router){}
+    isLoginMode = true;
 
+  
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('',[Validators.required,Validators.email]),
+    password: new FormControl('',[Validators.required,Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/)])
+  })
+
+   toggleMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  logIn(){
+    console.log(this.loginForm.value)
+    // if(this.loginForm.valid){
+    //    this._AuthServices.serLogIn(this.loginForm.value).subscribe({
+    //   next:(res)=>{
+    //         console.log(res);
+
+    //   }
+    // })
+    // }
+  }
 }
